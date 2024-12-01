80198f9f16873b738f5a2ffc380227f977b565e6
khd325 <hdk325@naver.com>
2024-12-01 17:58:45 +0900

    스레드 풀과 Executor 프레임워크2

--- src/thread/executor/ExecutorUtils.java
+++ src/thread/executor/ExecutorUtils.java
@@ -9,15 +9,28 @@ public abstract class ExecutorUtils {
 
     public static void printState(ExecutorService executorService) {
         if (executorService instanceof ThreadPoolExecutor poolExecutor) {
-            int pool = poolExecutor.getLargestPoolSize();
+            int pool = poolExecutor.getPoolSize();
             int active = poolExecutor.getActiveCount();
             int queuedTask = poolExecutor.getQueue().size();
             long completedTask = poolExecutor.getCompletedTaskCount();
 
-            log("[pool=" + pool + ", active=" + active +  ", queuedTask=" + queuedTask +
+            log("[pool=" + pool + ", active=" + active + ", queuedTask=" + queuedTask +
                     ", completedTask=" + completedTask + "]");
         } else {
             log(executorService);
+        }
+    }
+
+    public static void printState(ExecutorService executorService, String taskName) {
+        if (executorService instanceof ThreadPoolExecutor poolExecutor) {
+            int pool = poolExecutor.getPoolSize();
+            int active = poolExecutor.getActiveCount();
+            int queued = poolExecutor.getQueue().size();
+            long completedTask = poolExecutor.getCompletedTaskCount();
+            log(taskName + " -> [pool=" + pool + ", active=" + active + ", queuedTasks=" + queued +
+                    ", completedTasks=" + completedTask + "]");
+        } else {
+            log(taskName + " -> " + executorService);
         }
     }
 }
